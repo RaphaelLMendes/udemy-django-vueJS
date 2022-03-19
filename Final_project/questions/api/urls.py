0a3538b857122910ead227d6392a676e2d52422b
Raphael Lucas Mendes <rapha__mendes@hotmail.com>
2022-03-19 18:46:34 -0300

    FinalProj - added answer endpoints

--- Final_project/questions/api/urls.py
+++ Final_project/questions/api/urls.py
@@ -6,7 +6,24 @@ from questions.api import views as qv
 
 router = DefaultRouter()
 router.register(r"questions", qv.QuestionViewset)
+#router.register(r"answers", qv.AnswerViewset)
 
 urlpatterns = [
-    path('', include(router.urls) )
+    path('', include(router.urls) ),
+    path(
+        'questions/<slug:slug>/answers', 
+        qv.AnswerListAPIView.as_view(), 
+        name="answer-list" 
+        ),
+    path(
+        'questions/<slug:slug>/answer', 
+        qv.AnswerCreateView.as_view(), 
+        name="answer-create" 
+        ),
+    path(
+        'answers/<uuid:uuid>/', 
+        qv.AnswerRUDAPIView.as_view(), 
+        name="answer-RUD" 
+        ),
+    
 ]
--- Final_project/questions/api/views.py
+++ Final_project/questions/api/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.validators import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAuthorOrReadOnly
 
-from questions.models import Question
+from questions.models import Question, Answer
 
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, AnswerSerializer
 
 class QuestionViewset(viewsets.ModelViewSet):
     queryset = Question.objects.all().order_by("-created_at")
@@ -14,3 +16,31 @@ class QuestionViewset(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+    
+class AnswerCreateView(generics.CreateAPIView):
+    queryset = Answer.objects.all().order_by("-created_at")
+    serializer_class = AnswerSerializer
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get("slug")
+        question = get_object_or_404(Question, slug=kwarg_slug)
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("you have already answered this question!")
+        serializer.save(author=request_user, question=question)
+
+class AnswerRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permissions_classes = (IsAuthorOrReadOnly)
+    lookup_field = "uuid"
+
+class AnswerListAPIView(generics.ListAPIView):
+    queryset = Answer.objects.all().order_by("-created_at")
+    serializer_class = AnswerSerializer
+
+    def get_queryset(self):
+
+        kwarg_slug = self.kwargs.get("slug")
+
+        return Answer.objects.filter(question__slug=kwarg_slug).order_by("-created_at")
